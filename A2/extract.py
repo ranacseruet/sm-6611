57513267bb3b42255d9d68f44f73a90cb1e43ba8
Md Ali Ahsan Rana <rana_cse_ruet@yahoo.com>
2015-03-03 22:11:55 -0500

    * attribute linking to db basic implementation

--- A2/extract.py
+++ A2/extract.py
@@ -3,28 +3,43 @@ __author__ = 'Rana'
 import os
 import re
 
+from link_db import saveFileBug, saveFileAuthor
+
 #installed in system
 from git import Repo
 
 source_dir = '/Users/Rana/PycharmProjects/sm-6611/A2/data/src/'
 
 def getFileInfo(fileName):
+
+    ##find first created date/time
+    ##
+
     repo = Repo(source_dir)
     commits = list(repo.iter_commits(paths=fileName))
+
+    authors = set()
+    bugs    = set()
 
     for commit in commits:
         #retrieve commit details
         co = repo.commit(commit)
         author = co.author.name
-
-        ##save author
+        saveFileAuthor(fileName, author)
+        authors.add(author)
 
         #regex to identify bug
         m = re.search('BUG=([\d]+)', co.message)
         if m:
             bugId = m.group(1)
-            #save bug
-            print bugId
+            bugs.add(bugId)
+            ##open bug file and find creator/owner/collaborators
+            #print bugId
+            saveFileBug(fileName, bugId)
+
+    ##save bugs and authors according to fileName
+    #saveToDB(fileName, authors, bugs)
+    print bugs
     return
 
 for root, dirs, files in os.walk(source_dir):
@@ -32,4 +47,5 @@ for root, dirs, files in os.walk(source_dir):
         # retrieve info for file and save to db
         getFileInfo(root+file)
         break
+    print len(dirs)
     break
