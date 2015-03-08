2af0ec96397110cd7363b764dcd6349f0fa0da54
Kunwar Rattan <kunwarrattan@live.com>
2015-03-07 22:12:36 -0800

    Updated insertion

--- A2/database.py
+++ A2/database.py
@@ -26,41 +26,38 @@ def createTables():
    #    name VARCHAR(90)
    # );
     cursor.execute("CREATE SEQUENCE FILE_id_seq")
-    cursor.execute("CREATE TABLE FileTable(ID INT UNIQUE NOT NULL DEFAULT NEXTVAL('FILE_id_seq'), filename varchar(250) null)")
+    cursor.execute("CREATE TABLE FileTable(ID INT UNIQUE NOT NULL DEFAULT NEXTVAL('FILE_id_seq'), filename varchar(250) NOT null UNIQUE )")
     cursor.execute("CREATE SEQUENCE Author_id_seq")
-    cursor.execute("CREATE TABLE AuthorTable (Author_ID INT UNIQUE NOT NULL DEFAULT NEXTVAL('Author_id_seq'), authorname varchar(250) null)")
+    cursor.execute("CREATE TABLE AuthorTable (Author_ID INT UNIQUE NOT NULL DEFAULT NEXTVAL('Author_id_seq'), authorname varchar(250) not null UNIQUE)")
     cursor.execute("CREATE SEQUENCE BUG_id_seq")
-    cursor.execute("CREATE TABLE BugTable (BUG_ID INT UNIQUE NOT NULL DEFAULT NEXTVAL('BUG_id_seq'), bugID INT not null)")
+    cursor.execute("CREATE TABLE BugTable (BUG_ID INT UNIQUE NOT NULL DEFAULT NEXTVAL('BUG_id_seq'), bugID INT not null UNIQUE)")
     cursor.execute("CREATE SEQUENCE TABLE_id_seq")
     cursor.execute("CREATE TABLE BugTable_FileTable (TABLE_ID bigint NOT NULL DEFAULT NEXTVAL('TABLE_id_seq') , Bug_ID int NOT NULL REFERENCES BugTable(BUG_ID), File_ID int not null REFERENCES FileTable(ID), Author_ID INT not null REFERENCES AuthorTable(Author_ID))")
     conn.commit()
     return
 
 def insert_filename(filename1):
-    cursor.execute("INSERT INTO FileTable (filename) VALUES (%s)", (filename1))
+    cursor.execute('INSERT INTO FileTable (filename) VALUES (%s)', (filename1))
     conn.commit()
     return
 
-def insert_Author(authorname, filename):
-    cursor.execute("INSERT INTO AuthorTable (filename, authorname) VALUES (%s, %s)", (filename, authorname))
-    #cursor.execute("INSERT INTO t1 (f1, f2, f3) VALUES (%s, %s, %s)", (4, 1000, None))
-    # results = cursor.fetchall()
-    # for row in results:
-    #      f1, f2, f3  = row
-
-        # print("f1 = %s, f2 = %s, f3 = %s" % (f1, f2, f3))
-
+def insert_Author(authorname):
+    cursor.execute("INSERT INTO AuthorTable (authorname) VALUES (%s)", (authorname))
     conn.commit()
     return
 
-def insert_Bug(filename,bugid):
-    cursor.execute("INSERT INTO BugTable (filename, bugid) VALUES (%s, %s)", (filename, bugid))
+def insert_Bug(bugid):
+    cursor.execute("INSERT INTO BugTable (bugID) VALUES (%s)", (bugid))
+    cursor.commit()
     return
 
-#def insert_
+def insert_Main_Table(filename1,authorname,bugid):
+    cursor.execute("INSERT INTO BugTable_FileTable(Bug_ID,File_ID,Author_ID) VALUES (%s,%s,%s)", (filename1,authorname,bugid))
+    cursor.commit()
+    return
 
-
-createTables()
-#insert_filename("Hello")
-#insert_Author("kunwar","rattan")
-#insert_Bug("kunwar",1234)
+#createTables()
+insert_filename(filename)
+insert_Author(authorname)
+insert_Bug(bugid)
+insert_Main_Table(filename,authorname,bugid)
