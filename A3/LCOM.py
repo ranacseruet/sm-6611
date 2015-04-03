1390ec853db7cb6095e278b168a7793773ece75d
Kunwar Rattan <kunwarrattan@live.com>
2015-04-03 19:12:44 -0400

    * update readme.md

--- A3/LCOM.py
+++ A3/LCOM.py
@@ -1,3 +1,32 @@
 __author__ = 'india'
 
+import understand
 
+#Open Database
+db = understand.open("E:\sm-6611\TestDatabase.udb")
+
+def sortKeyFunc(ent):
+  return str.lower(ent.longname())
+#
+# ents = db.ents("function,method,procedure")
+# for func in sorted(ents,key = sortKeyFunc):
+#   print (func.longname()," (",sep="",end="")
+#   first = True
+#   for param in func.ents("Define","Object"):
+#     if not first:
+#       print (", ",end="")
+#     print (param.type(),param,end="")
+#     first = False
+#   print (")")
+
+
+ents = db.ents("function,method,procedure")
+for func in sorted(ents,key = sortKeyFunc):
+  print (func.longname()," (",sep="",end="")
+  first = True
+  for param in func.ents("use by","Function"):
+    if not first:
+      print (", ",end="")
+    print (param.type(),param,end="")
+    first = False
+  print (")")
