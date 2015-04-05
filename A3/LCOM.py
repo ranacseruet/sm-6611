d36be7fba02de457629bac3e42114de7062b6f3a
Md Ali Ahsan Rana <rana_cse_ruet@yahoo.com>
2015-04-05 14:56:12 -0400

    * getting method dependencies.

--- A3/LCOM.py
+++ A3/LCOM.py
@@ -2,6 +2,7 @@ __author__ = 'india'
 
 import os
 import sys
+
 import understand
 
 #Open Database
@@ -10,14 +11,48 @@ db = understand.open("/Users/Rana/UnderStandProjects/TestProject.udb")
 
 
 
+def dependsOn(func1, func2):
+
+    #func1 dependents list building is happening everytime unnecessarily
+
+    #TODO check attribute dependency
+
+    relatedFunctions = func1.ents("","function,method,procedure")
+
+    if relatedFunctions == None:
+        return False
+
+    fList = []
+    for func in relatedFunctions:
+        if func.library() == "Standard":
+            continue
+        if func.name() != func1.name():
+            fList.append(func.name())
+
+    if func2.name() in fList:
+        print(func1.name(), "depends on", func2.name())
+        return True
+    else:
+        return False
+
+
 def getMethodsByFile(file):
 
     functions  = file.ents("","function,method,procedure")
+    funcSet = set()
 
     for func1 in functions:
+        if func1.library() == "Standard":
+            continue
         for func2 in functions:
-            if(func1 != func2):
-                print(func1, " and ", func2)
+            if func2.library() == "Standard":
+                continue
+            if(func1 != func2) & dependsOn(func1, func2):
+                # (a,b) and (b,a) both are being added
+                funcSet = funcSet | {(func1.name(), func2.name())}
+
+    #for pair in funcSet:
+    #    print(pair)
 
     return
 
