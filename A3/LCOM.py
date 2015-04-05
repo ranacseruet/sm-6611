30d8432e064380999884771c9ee1c503bcba628f
Md Ali Ahsan Rana <rana_cse_ruet@yahoo.com>
2015-04-05 14:09:25 -0400

    * Getting method pairs for each cpp file.

--- A3/LCOM.py
+++ A3/LCOM.py
@@ -1,32 +1,33 @@
 __author__ = 'india'
 
+import os
+import sys
 import understand
 
 #Open Database
-db = understand.open("E:\sm-6611\TestDatabase.udb")
-
-def sortKeyFunc(ent):
-  return str.lower(ent.longname())
-#
-# ents = db.ents("function,method,procedure")
-# for func in sorted(ents,key = sortKeyFunc):
-#   print (func.longname()," (",sep="",end="")
-#   first = True
-#   for param in func.ents("Define","Object"):
-#     if not first:
-#       print (", ",end="")
-#     print (param.type(),param,end="")
-#     first = False
-#   print (")")
+db = understand.open("/Users/Rana/UnderStandProjects/TestProject.udb")
 
 
-ents = db.ents("function,method,procedure")
-for func in sorted(ents,key = sortKeyFunc):
-  print (func.longname()," (",sep="",end="")
-  first = True
-  for param in func.ents("use by","Function"):
-    if not first:
-      print (", ",end="")
-    print (param.type(),param,end="")
-    first = False
-  print (")")
+
+
+def getMethodsByFile(file):
+
+    functions  = file.ents("","function,method,procedure")
+
+    for func1 in functions:
+        for func2 in functions:
+            if(func1 != func2):
+                print(func1, " and ", func2)
+
+    return
+
+
+for file in db.ents("File"):
+
+  if(file.library() == "Standard"):
+      continue
+
+  fileName, fileExtension = os.path.splitext(str(file))
+  if fileExtension == ".cpp":
+    getMethodsByFile(file)
+
