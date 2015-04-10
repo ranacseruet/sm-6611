8c5e9ae4c2e4c2f581bc5de04dcf80a09c41bd54
Md Ali Ahsan Rana <rana_cse_ruet@yahoo.com>
2015-04-09 23:00:02 -0400

    * consider only files with class defined.

--- A3/main.py
+++ A3/main.py
@@ -11,8 +11,8 @@ from db import SaveToDB
 if __name__ == "__main__":
     start = datetime.datetime.now()
 
-    #versions = ["34.0.1847.0"]
-    versions = ["34.0.1847.0", "33.0.1750.0", "32.0.1700.0", "31.0.1650.0", "30.0.1599.0", "29.0.1547.0", "28.0.1500.0", "27.0.1453.0", "26.0.1410.0", "25.0.1364.0"]
+    versions = ["34.0.1847.0"]
+    #versions = ["34.0.1847.0", "33.0.1750.0", "32.0.1700.0", "31.0.1650.0", "30.0.1599.0", "29.0.1547.0", "28.0.1500.0", "27.0.1453.0", "26.0.1410.0", "25.0.1364.0"]
     # "34.0.1847.0" "33.0.1750.0"    32.0.1700.0    31.0.1650.0     30.0.1599.0     29.0.1547.0     28.0.1500.0     27.0.1453.0     26.0.1410.0     25.0.1364.0
 
     for version in versions:
@@ -26,11 +26,14 @@ if __name__ == "__main__":
 
           fileName, fileExtension = os.path.splitext(str(file))
           if fileExtension == ".cpp" or fileExtension == ".cc" :
-            lcom = getLCOM(file)
-            cbo = getCBO(file)
-            #print("Version: ", version ,"File: ",file.longname(), " LCOM: ",lcom, " CBO: ", cbo)
-            #DB CALL need to save: version, file, lcom, CBO
-            SaveToDB(version, file.longname(), lcom, cbo)
+            classes = file.ents("Define","Class")
+            for cls in classes:
+                lcom = getLCOM(cls)
+                cbo = getCBO(cls)
+                #print("Version: ", version ,"File: ",file.longname()+":"+cls.longname(), " LCOM: ",lcom, " CBO: ", cbo)
+                #DB CALL need to save: version, file, lcom, CBO
+                SaveToDB(version, file.longname(), lcom, cbo)
+
         end = datetime.datetime.now()
         print("total time: ", end-start)
         #print(getClassKind())
